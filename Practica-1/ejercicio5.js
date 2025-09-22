c8354f774f4651b617c61efb1e5131227c8d98bd
alesslim <153566282+alesslim@users.noreply.github.com>
2025-09-22 08:37:16 -0400

    Update ejercicio5.js

--- Practica-1/ejercicio5.js
+++ Practica-1/ejercicio5.js
@@ -1,7 +1,6 @@
 // ===================================================
 // Caratula
 // Nombre Completo: Limachi Colque Felipe Alessandro
-// C.I.: 9120895
 // Carrera: Informatica
 // Materia: Programacion web 3
 // Fecha: 20/09/2025
@@ -26,4 +25,5 @@ let miFuncion = x =>{
 let band = miFuncion('Oruro');
 console.log(band);
 let band1 = miFuncion('Hola');
+
 console.log(band1);
