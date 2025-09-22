932a245058c990a2ee9e34a3ab2fd4ff28eb6294
alesslim <153566282+alesslim@users.noreply.github.com>
2025-09-22 08:35:44 -0400

    Update ejercicio1.js

--- Practica-1/ejercicio1.js
+++ Practica-1/ejercicio1.js
@@ -1,7 +1,6 @@
 // ===================================================
 // Caratula
 // Nombre Completo: Limachi Colque Felipe Alessandro
-// C.I.: 9120895
 // Carrera: Informatica
 // Materia: Programacion web 3
 // Fecha: 20/09/2025
@@ -36,4 +35,5 @@ let miFuncion = x =>{
 };
 
 let obj = miFuncion("euforia");
+
 console.log(obj);
